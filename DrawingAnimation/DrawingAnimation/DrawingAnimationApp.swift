478f87ea41df75d3fa7ee06e2d45362ee05475df
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-20 19:03:35 +0400

    Add files via upload

---
+++ DrawingAnimation/DrawingAnimation/DrawingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAnimationApp.swift
+//  DrawingAnimation
+//
+//  Created by Abdurokhman Mukhanbetov on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
